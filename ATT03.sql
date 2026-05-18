7a1e64ac644bd721366a81ca84cdf19b7fa2af9b
Julia Renata Soares <julia.soares@selbetti.local>
2026-05-18 14:35:57 -0300

    subindo atualização de conteúdo

--- ATT03.sql
+++ ATT03.sql
@@ -1,26 +1,24 @@
 /* Exercícios Capítulo 05. Use o SGBD para fazer.
-
 Exercício 1
-
 - Consulte todos os professores que trabalham na universidade em que a especialidade seja 'Matematica',
-
 'Literatura'. Ordene (na forma CRESCENTE) pelo nome do professor.
-
 Use a tabela PROFESSOR.
-
-
-
-Exercício 2
-
-- Consulta todas as notas dos alunos
-
-Ordene o resultado pela nota na forma DECRESCENTE.
-
-
-Exercício 3
-
-- Traga todas as especialidades dos professores (não repita os resultados)
-
-Dica: use o operador DISTINCT e a tabela PROFESSOR.
-
 */
+SELECT * FROM PROFESSOR
+WHERE especialidade IN ('Matematica', 'Literatura') 
+ORDER BY nome ASC
+
+/*Exercício 2
+- Consulta todas as notas dos alunos
+Ordene o resultado pela nota na forma DECRESCENTE.*/
+
+SELECT * FROM FREQUENTA
+ORDER BY nota DESC  
+
+/*
+Exercício 3
+- Traga todas as especialidades dos professores (não repita os resultados)
+Dica: use o operador DISTINCT e a tabela PROFESSOR.
+*/
+
+SELECT DISTINCT especialidade FROM PROFESSOR
